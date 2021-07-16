e306a2270d01a93a761762f4589b905c079b1722
Inna <dsino@naver.com>
2021-07-17 04:04:48 +0900

    m

--- src/main/java/kr/ac/kopo/kopo08/web/BoardItemController.java
+++ src/main/java/kr/ac/kopo/kopo08/web/BoardItemController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
